e461c87847680fdaa80ce28be826ef272b8eb782
Danil Masalov <dairen12121212@gmail.com>
2023-09-28 22:37:47 +0700

    hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
+import { HashRouter } from "react-router-dom";
 
-import ReactDOM from 'react-dom/client'
-import App from './components/App/App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './store/store.js'
-import { BrowserRouter } from 'react-router-dom'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
   // </React.StrictMode>,
-)
+);
